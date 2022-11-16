3acd40e2b82f37b2fc064dac8d78d05d4b90270d
Monero-HackerIndustrial <106575456+Monero-HackerIndustrial@users.noreply.github.com>
2022-11-15 22:30:30 -0600

    slight UI changes. Clean up library code

--- src/main.py
+++ src/main.py
@@ -1,60 +1,13 @@
-# from libs.moneroSignerLib import Companion
-
 from ui.companionUI import companionUI
 
-# import tkinter as tk
-# from tkinter import *
-import tkinter
-import tkinter.filedialog
-from tkinter import ttk
 
 
-import pathlib
+print("Monerosigner Companion")
+print("Crowdfunded by the Monero CCS")
 
-
-
-
-
-
-print("Future home of the companion application")
-
-def setupMenu(_master):
-    menubar = tkinter.Menu(_master)
-    menubar.config(bg="white")
-    _master.config(menu=menubar)
-
-    fileMenu = tkinter.Menu(menubar)
-    fileMenu.add_command(label="Exit", command=_master.quit())
-    menubar.add_cascade(label="File", menu=fileMenu)
-
-    helpMenu = tkinter.Menu(menubar)
-    helpMenu.add_command(label="about", command=lambda:print("about application"))
-    menubar.add_cascade(label="Help", menu=helpMenu)
-
-def selectFile():
-    file = tkinter.filedialog.askopenfile(mode='r')
-    if file:
-        print(file.name)
-        p = pathlib.Path(file.name)
-        print(p)
-        return p
-    else:
-        return False
-
-def grids():
-    x = rootUI.grid_size()
-    print(x)
 
 
 
 
 UI = companionUI()
 print(UI.getVersion())
-
-
-
-setupMenu(UI.rootUI)
-
-
-
-UI.rootUI.mainloop()
--- src/ui/companionUI.py
+++ src/ui/companionUI.py
@@ -1,8 +1,7 @@
 import tkinter
 import tkinter.filedialog
 from tkinter import ttk
-
-
+#Needed for normalizing the filepath between different OSes.
 import pathlib
 
 
@@ -10,6 +9,8 @@ class companionUI:
     def __init__(self):
         self.setupUI()
         self.version = "0.0.1"
+        self.file2Send = None
+        self.file2Recieve = None
     def getVersion(self):
         return self.version
     def setupUI(self):
@@ -21,12 +22,12 @@ class companionUI:
         self.setupSendFrame()
         self.setupDivider()
         self.setuprecieveFrame()
+        self.rootUI.mainloop()
     # Setup menu sets up the file menu dialog
     def setupMenu(self):
         menubar = tkinter.Menu(self.rootUI)
         menubar.config(bg="white")
         self.rootUI.config(menu=menubar)
-
         fileMenu = tkinter.Menu(menubar)
         fileMenu.add_command(label="Exit", command=self.rootUI.quit())
         menubar.add_cascade(label="File", menu=fileMenu)
@@ -42,10 +43,13 @@ class companionUI:
         self.rootUI.grid_columnconfigure(0,weight=1)
         # sendFrame.config(bg="black")
         sendHeader = tkinter.Label(sendFrame, text="Send to Monerosigner", height=1, font=('TkFixedFont', 16))
-        sendHeader.grid(row=0, column=0, pady=(5,5),  columnspan=3)
+        sendHeader.grid(row=0, column=0, pady=(5,10),  columnspan=3)
         #setup the file select
+        sendFileSelectLabel = tkinter.Label(sendFrame, text="Unsigned Transaction to send:", height=0, font=('TkFixedFont', 10))
+        sendFileSelectLabel.grid(row=2, column=0,  columnspan=2)
+
         sendSelectFile = tkinter.Button(sendFrame, text="selectTx", command=self.selectFile).grid(row=2, column=4)
-        sendStartSending = tkinter.Button(sendFrame, text="start", command=self.grids).grid(row=3, column=4, sticky=tkinter.EW)
+        sendStartSending = tkinter.Button(sendFrame, text="start", command=self.send2signer).grid(row=3, column=4, sticky=tkinter.EW)
     def setupDivider(self):
         # sets up a divider UI element between the send and recieveFrame
         divider = tkinter.ttk.Separator(self.rootUI, orient=tkinter.HORIZONTAL).grid(row=1,column=0,pady=(5,5), columnspan=12, sticky=tkinter.EW)
@@ -56,8 +60,11 @@ class companionUI:
         recieveFrame.grid_columnconfigure(0,weight=1)
         recieveHeader = tkinter.Label(recieveFrame, text="Recieve from Monerosigner", height=1, font=('TkFixedFont', 16))
         recieveHeader.grid(row=0, column=0, pady=(5,5), columnspan=3)
+        #label for recieve buttons
+        recieveFileSelectLabel = tkinter.Label(recieveFrame, text="Where to save Signed TX:", height=0, font=('TkFixedFont', 10))
+        recieveFileSelectLabel.grid(row=2, column=0,  columnspan=2)
         #revieve and save
-        recieveSelectFile = tkinter.Button(recieveFrame, text="selectTx", command=self.selectFile).grid(row=2, column=4)
+        recieveSelectFile = tkinter.Button(recieveFrame, text="txName", command=self.selectFile).grid(row=2, column=4)
 
         recieveStartSending = tkinter.Button(recieveFrame, text="start", command=self.grids).grid(row=3, column=4, sticky=tkinter.EW)
 
@@ -67,6 +74,7 @@ class companionUI:
         if file:
             p = pathlib.Path(file.name)
             print(p)
+            self.file2Send = p
             return p
         else:
             return False
@@ -74,3 +82,5 @@ class companionUI:
     def grids(self):
         x = self.rootUI.grid_size()
         print(x)
+    def send2signer(self):
+        print(self.file2Send)
